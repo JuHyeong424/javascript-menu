8caffaf2e866f46181a42b5f27bdb0e92fb746d5
JuHyeong424 <wkdwngud963@gmail.com>
2026-01-06 20:16:19 +0900

    feat:최대 못 먹는 음식 수 오류 처리

--- src/view/inputView.js
+++ src/view/inputView.js
@@ -1,4 +1,5 @@
 import {Console} from "@woowacourse/mission-utils";
+import {printError} from "./outputView.js";
 
 export async function inputCoachName() {
   return await Console.readLineAsync('코치의 이름을 입력해 주세요. (, 로 구분)\n')
@@ -8,7 +9,17 @@ export async function inputNotEatMenu(coachNameArray) {
   const NotEatMenuObject = {};
 
   for (const name of coachNameArray) {
-    NotEatMenuObject[name] = await Console.readLineAsync(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`);
+    while (true) {
+      try {
+        NotEatMenuObject[name] = await Console.readLineAsync(`${name}(이)가 못 먹는 메뉴를 입력해 주세요.\n`);
+        if (Object.values(NotEatMenuObject[name]).length < 0 || Object.values(NotEatMenuObject[name]).length > 2) {
+          throw new Error('[ERROR]못 먹는 메뉴는 0~2개 사이입니다.');
+        }
+        break;
+      } catch (e) {
+        await printError(e.message);
+      }
+    }
   }
 
   return NotEatMenuObject;
